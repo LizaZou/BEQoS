1df24e02f9f2fb45b66964ce9cb4384aeff0632c
Elisabeth <elisabeth.maton@etu.iut-tlse3.fr>
2024-05-22 20:23:04 +0200

    Archi2

--- ResaPacket.java
+++ ResaPacket.java
@@ -6,13 +6,46 @@ public class ResaPacket {
     private int idResa;
     private InetAddress ipDest;
     private InetAddress ipSource;
-    private float debit;
+    private float debitRequest;
     private InetSocketAddress portDest;
     private String protocol;
     private String classTrafic;
 
-    public ResaPacket(InetAddress ipDest, InetAddress ipSource, ){
-        
+    public ResaPacket(InetAddress ipD, InetAddress ipS, float debitRequest, InetSocketAddress port, String protocol, String classTrafic){
+        this.ipDest=ipD;
+        this.ipSource=ipS;
+        this.debitRequest=debitRequest;
+        this.portDest=port;
+        this.protocol=protocol;
+        this.classTrafic=classTrafic;
+    }
+
+    public int getIdResa(){
+        return this.idResa;
+    }
+
+    public InetAddress getIpDest(){
+        return this.ipDest;
+    }
+
+    public InetAddress getIpSource(){
+        return this.ipSource;
+    }
+
+    public float getDebitRequest(){
+        return this.debitRequest;
+    }
+
+    public InetSocketAddress getPortDest(){
+        return this.portDest;
+    }
+
+    public String getProtocol(){
+        return this.protocol;
+    }
+
+    public String getClassTrafic(){
+        return this.classTrafic;
     }
 
 
